392c45fa204d8965fde80bf7b5c057080a28d5d1
Cheolhyun Mun <zooxop@gmail.com>
2023-11-04 12:47:48 +0900

    add: basic data types

---
+++ data_types/basic-datatypes.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name = 'mch'; // "" 써도 됨.
+  bool alive = true;
+  int age = 12;
+  double money = 55.44;
+
+  // `num`: int, double 등이 상속받은 클래스(데이터 타입)
+  // num 타입은 int 일수도, double일 수도 있음.
+  num x = 12;
+
+  x = 1.1; // int값을 double로 바꾸는 것이 가능함.
+}
